2f6b31aa87f811dd2cf048e97e6546cc25c3ca53
RoyannX <136560573+RoyannX@users.noreply.github.com>
2023-06-22 12:26:07 +0700

    Add files via upload

---
+++ Biodata_Royan.py
@@ -0,0 +1,32 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Muhammad Royan"
+hobi = "Membantu mengoptimalkan \n                skill programming"
+alamat = "Gaduang permai"
+Kota = "cianjur"
+Propinsi = "Jawa Barat"
+
+print("Nama Lengkap  :",nama)
+
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Royan"
+hobi = "makan \n                gorengan"
+alamat = "gadung permai"
+Kota = "cianjur"
+Propinsi = "Jawa Barat"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 
